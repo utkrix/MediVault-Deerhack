547dfb4a3a501f63abb789071970a00d10c75785
utkrix <utkrist991@gmail.com>
2024-05-10 22:03:21 +0545

    test data received

--- json_deserializer/json_deserializer.ino
+++ json_deserializer/json_deserializer.ino
@@ -34,12 +34,37 @@ void loop() {
 
 void sendData(){
     StaticJsonDocument<300> JSONData;
-    // Use the object just like a javascript object or a python dictionary
-    JSONData["key"] = "Value";
-    // You can add more fields
+    JSONData["key"] = "Value"; // like dictionary
+    //more fields
     char data[300];
-    // Converts the JSON object to String and stores it in data variable
+    // Convert JSON object to String and stores it in data variable
     serializeJson(JSONData,data);
     // Set status code as 200, content type as application/json and send the data
   server.send(200,"application/json",data);
+}
+
+
+
+void receiveData(){
+  StaticJsonDocument<300> JSONData;
+
+  String jsonString = server.arg("plain");
+  DeserializationError error = deserializeJson(JSONData, jsonString);
+
+
+  if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.f_str());
+    server.send(500,"application/json","Error in parsing"); 
+    return;
+
+  }
+  else{
+   if(JSONData.containsKey("number")){ 
+    server.send(200,"application/json",String(JSONData["number"].as<int>())+" Received");
+   }
+   else{
+     server.send(400,"application/json","Bad JSON");
+   }
+  }
 }
