de696f531f86e8581017843ab944beb0583b9b7c
utkrix <utkrist991@gmail.com>
2024-05-11 00:49:14 +0545

    Time iteration and led active

---
+++ nodeFinal/nodeFinal.ino
@@ -0,0 +1,130 @@
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+
+const char *ssid     = "DWIT-Hotspot";
+const char *password = "@DWZone-hotspot1";
+
+#define led D4
+
+
+ESP8266WebServer server(80);
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  WiFi.begin(ssid,password);
+    while(WiFi.status() != WL_CONNECTED){
+      Serial.println("Connecting");
+    delay(1000);
+    }
+    Serial.println("Connected to");
+    Serial.println(WiFi.localIP());
+
+    server.on("/test",HTTP_GET,sendData);
+
+    server.on("/test",HTTP_POST,receiveData);
+
+    server.begin();
+
+}
+
+void loop() {
+    server.handleClient();
+}
+
+// to send data to api
+
+void sendData(){
+    StaticJsonDocument<300> JSONData;
+    JSONData["key"] = "Value"; // like dictionary
+    //more fields
+    char data[300];
+    // Convert JSON object to String and stores it in data variable
+    serializeJson(JSONData,data);
+    // Set status code as 200, content type as application/json and send the data
+  server.send(200,"application/json",data);
+}
+
+
+
+void receiveData(){
+  StaticJsonDocument<300> JSONData;
+
+  String jsonString = server.arg("plain");
+  DeserializationError error = deserializeJson(JSONData, jsonString);
+
+
+  if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.f_str());
+    server.send(500,"application/json","Error in parsing"); 
+    return;
+
+  }
+  const char* name = JSONData["name"];
+  Serial.println(name);
+
+  JsonArray dayArray = JSONData["day"];
+  int dayArraySize =  dayArray.size();
+  // Serial.println(dayArraySize);
+  String day[dayArraySize];
+
+  for (int i = 0; i < dayArraySize; i++){
+    // day data stored in day array
+
+    day[i] = dayArray[i].as<String>();
+  }
+
+  //traverse and find correct to blink
+  Serial.println("Days:");
+  for (int i = 0; i < dayArraySize; i++) {
+  Serial.println(day[i]);
+}
+
+  JsonArray timeArray = JSONData["time"];
+  int timeArraySize =  timeArray.size();
+  // Serial.println(timeArraySize);
+  String time[timeArraySize];
+
+  Serial.println("Timings:");
+  for (int i = 0; i < timeArray.size(); i++) {
+    // data stored in time array
+    time[i] = timeArray[i].as<String>();
+  }
+  // int highest = 0;
+
+  // if (timeArraySize > dayArraySize){
+  //   highest = timeArraySize;
+  // }
+  // else{
+  //   highest = dayArraySize;
+  // }
+// traverse later and find correct to blinkr
+
+  for (int i = 0; i < timeArraySize; i++) {
+    Serial.println(time[i]);
+  }
+
+  for (int i = 0; i < dayArraySize; i++)
+  {
+    if (day[i] == "Monday")
+    {
+      for (int o = 0; o < timeArraySize; o++)
+      {
+        if (time[o] == "22:00:00")
+        {
+          for(int n = 0; n < 15; n++)
+          {
+            digitalWrite(led, HIGH);
+            delay(300);
+            digitalWrite(led, LOW);
+            delay(300);
+          }
+        }
+      }
+    }
+  }
+}
