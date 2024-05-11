c2b8596e4249f53924ee6b1f6de5369b2b71765a
utkrix <utkrist991@gmail.com>
2024-05-11 12:02:07 +0545

    multiple led blink ready

--- nodeFinal/nodeFinal.ino
+++ nodeFinal/nodeFinal.ino
@@ -3,59 +3,102 @@
 #include <ESP8266WebServer.h>
 #include <WiFiUdp.h>
 
-const char *ssid     = "DWIT-Hotspot";
+const char *ssid = "DWIT-Hotspot";
 const char *password = "@DWZone-hotspot1";
 
-#define led D4
+#define led1 D4
+#define led2 D5
+
 #define touch1 D7
 #define touch2 D8
+
+unsigned long previousMillis = 0;
+const long interval = 300;
 
 
 ESP8266WebServer server(80);
 
+int dayArraySize;
+int timeArraySize;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
   pinMode(touch1, INPUT);
   pinMode(touch2, INPUT);
-  WiFi.begin(ssid,password);
-    while(WiFi.status() != WL_CONNECTED){
-      Serial.println("Connecting");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Connecting");
     delay(1000);
-    }
-    Serial.println("Connected to");
-    Serial.println(WiFi.localIP());
+  }
+  Serial.println("Connected to");
+  Serial.println(WiFi.localIP());
 
-    server.on("/test",HTTP_GET,sendData);
+  server.on("/test", HTTP_GET, sendData);
 
-    server.on("/test",HTTP_POST,receiveData);
+  server.on("/test", HTTP_POST, receiveData);
 
-    server.begin();
-
+  server.begin();
 }
 
 void loop() {
-    server.handleClient();
+  server.handleClient();
 }
 
 // to send data to api
 
-void sendData(){
-    StaticJsonDocument<300> JSONData;
-    JSONData["key"] = "Value"; // like dictionary
-    //more fields
-    char data[300];
-    // Convert JSON object to String and stores it in data variable
-    serializeJson(JSONData,data);
-    // Set status code as 200, content type as application/json and send the data
-  server.send(200,"application/json",data);
+void sendData() {
+  StaticJsonDocument<300> JSONData;
+  JSONData["key"] = "Value";  // like dictionary
+  //more fields
+  char data[300];
+  // Convert JSON object to String and stores it in data variable
+  serializeJson(JSONData, data);
+  // Set status code as 200, content type as application/json and send the data
+  server.send(200, "application/json", data);
 }
 
+void touchSensor(){  
+  int state1 = digitalRead(touch1);
+  Serial.println(state1);
+  int state2 = digitalRead(touch2);
+  Serial.println(state2);
+  if (state1 == 1 && state2 == 1) {
+    Serial.println("Touched!");
+}
 
+}
 
-void receiveData(){
-    String jsonString = server.arg("plain");
+// Function to handle LED blinking
+void handleLED(int led) {
+  
+  // code for millis NA
+
+  // const long interval = 300;
+  // static bool ledState = LOW; // Initial state: LOW (off)
+  // static unsigned long millisSinceLastBlink = 0; // Time since last blink
+  // unsigned long currentMillis = millis();
+
+  // // Check if it's time to toggle (based on interval)
+  // if (currentMillis - millisSinceLastBlink >= interval) {
+  //   millisSinceLastBlink = currentMillis;
+
+  //   // Toggle LED state and control pin
+  //   ledState = !ledState;
+  //   digitalWrite(led, ledState);
+  // }
+  for (int i=0; i < 15; i++)
+  {
+    digitalWrite(led, HIGH);
+    delay(300);
+    digitalWrite(led, LOW);
+    delay(300);
+  }
+}
+
+void receiveData() {
+  String jsonString = server.arg("plain");
   Serial.println("Received JSON String:");
   Serial.println(jsonString);
 
@@ -79,9 +122,9 @@ void receiveData(){
   for (int i = 0; i < dataSize; i++) {
     JsonObject obj = dataArray[i];
     Serial.print("Processing data set ");
-    Serial.println(i + 1); // Print index starting from 1
+    Serial.println(i + 1);  // Print index starting from 1
 
-    const char* name = obj["name"];
+    const char *name = obj["name"];
     Serial.print("Name: ");
     Serial.println(name);
 
@@ -95,44 +138,27 @@ void receiveData(){
     }
 
     JsonArray timeArray = obj["time"];
-    int timeArraySize = timeArray.size();
+    timeArraySize = timeArray.size();
     Serial.print("Number of times: ");
     Serial.println(timeArraySize);
     Serial.println("Times:");
     for (int j = 0; j < timeArraySize; j++) {
       Serial.println(timeArray[j].as<String>());
     }
-  
-  for (int i = 0; i < dayArraySize; i++)
-  {
-    if (dayArray[i] == "Tuesday")
-    {
-      for (int o = 0; o < timeArraySize; o++)
-      {
-        if (timeArray[o] == "14:00:00")
-        {
-          for(int n = 0; n < 15; n++)
-          {
-            digitalWrite(led, HIGH);
-            delay(300);
-            digitalWrite(led, LOW);
-            delay(300);
+    for (int i = 0; i < dayArraySize; i++) {
+      if (dayArray[i] == "Tuesday") {
+        for (int o = 0; o < timeArraySize; o++) {
+          if (timeArray[o] == "14:00:00") {
+            handleLED(led1);
+            touchSensor();
+            // checking if sensor is activated after consumption
+            }
+          if (timeArray[o] == "23:00:00"){
+            handleLED(led2);
+            touchSensor();
           }
-          
-          // checking if sensor is activated after consumption
-          int state1 = digitalRead(touch1);
-          Serial.println(state1);
-          int state2 = digitalRead(touch2);
-          Serial.println(state2);
-          if (state1 == state2 == 1)
-          {
-            Serial.println("Touched!");
           }
-
         }
       }
-    }
-  }
   }
 }
-
